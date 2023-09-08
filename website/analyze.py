5a9a509a5066d417d240394e8e09a404a8ea651e
notBigL <99298254+notBigL@users.noreply.github.com>
2023-09-08 15:56:57 +0200

    User stuff is deprecated now and the website can now analyse the sentiment and irony of a given text. Output is only on the console tho rn

---
+++ website/analyze.py
@@ -0,0 +1,26 @@
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers_interpret import SequenceClassificationExplainer
+
+
+def analyze_sentiment(data):
+    model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    cls_explainer = SequenceClassificationExplainer(model, tokenizer)
+    cls_explainer(data)
+
+    print(cls_explainer.predicted_class_name, flush=True)
+    print(cls_explainer.word_attributions, flush=True)
+
+
+def analyze_irony(data):
+    model_name = "cardiffnlp/twitter-roberta-base-irony"
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    cls_explainer = SequenceClassificationExplainer(model, tokenizer)
+    cls_explainer(data)
+
+    print(cls_explainer.predicted_class_name, flush=True)
+    print(cls_explainer.word_attributions, flush=True)
--- website/views.py
+++ website/views.py
@@ -4,25 +4,23 @@ from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
+from . import analyze
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
-@login_required
 def home():
     if request.method == 'POST':
         note = request.form.get('note')
 
-        if len(note) < 1:
-            flash("Note is too short", category='error')
+        if len(note) < 6:
+            flash("Text is too short", category='error')
         else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
+            analyze.analyze_sentiment(note)
             flash("Note added", category='success')
 
-    return render_template("home.html", user=current_user)
+    return render_template("home.html")
 
 
 @views.route("/delete-note", methods=["POST"])
@@ -36,3 +34,31 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
+
+
+@views.route('/sentiment', methods=['GET', 'POST'])
+def sentiment():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 6:
+            flash("Text is too short", category='error')
+        else:
+            analyze.analyze_sentiment(note)
+            flash("Completed analysis", category='success')
+
+    return render_template("sentiment.html")
+
+
+@views.route('/irony', methods=['GET', 'POST'])
+def irony():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 6:
+            flash("Text is too short", category='error')
+        else:
+            analyze.analyze_sentiment(note)
+            flash("Completed analysis", category='success')
+
+    return render_template("irony.html")
