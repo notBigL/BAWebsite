8c81f271cbb7974220019707157de68bf42b75d6
notBigL <99298254+notBigL@users.noreply.github.com>
2023-09-08 13:11:00 +0200

    finished the tutorial, logging in, writing notes and deleting them works perfectly now. Jippie

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -12,6 +13,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+
     from .views import views
     from .auth import auth
 
@@ -22,10 +24,19 @@ def create_app():
 
     create_database(app)
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print("Created DB Yattaa")
+    if not path.exists('instance/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print("Created DB!")
--- website/auth.py
+++ website/auth.py
@@ -1,34 +1,65 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Password incorrect', category='error')
+        else:
+            flash('Email does not exist', category='error')
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("Email is already in use", category='error')
+        elif len(email) < 4:
             flash("Email must be greater than 3 characters", category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash("First name must be greater than 1 character", category='error')
         elif len(password1) < 7:
             flash("Password must be greater than 6 characters", category='error')
         elif password1 != password2:
             flash("Passwords dont match sadly uwu", category='error')
         else:
+            new_user = User(email=email, first_name=first_name,
+                            password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash("Yay you did it mane", category='success')
-    return render_template("sign_up.html")
+            login_user(user, remember=True)
+            return redirect(url_for('views.home'))
+    return render_template("sign_up.html", user=current_user)
