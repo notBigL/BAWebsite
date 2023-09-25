99f39112c6d4196fc52257c42bdedf9185c18f8a
notBigL <99298254+notBigL@users.noreply.github.com>
2023-09-25 14:10:07 +0200

    moved a lot of stuff so vercel can see it

---
+++ api/website/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+
+
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
+    return app
