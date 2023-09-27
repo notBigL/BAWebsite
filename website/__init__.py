1b14f58069aa0414d340394b1ff7bf1a934b7944
notBigL <99298254+notBigL@users.noreply.github.com>
2023-09-27 09:53:36 +0200

    Deleted database

--- website/__init__.py
+++ website/__init__.py
@@ -1,19 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from os import path
-
-db = SQLAlchemy()
-DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config['SECRET_KEY'] = 'burgerflipp'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
-
 
     from .views import views
 
